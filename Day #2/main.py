b39fec81f54fef0d1a16b456ef93919471911b6a
Baker2002 <bakirhimzanija@gmail.com>
2022-09-01 22:14:41 +0200

    Finished

--- Day #2/main.py
+++ Day #2/main.py
@@ -2,14 +2,15 @@ import random
 
 list = []
 list2 = []
+
 for a in range(0,5):
     list.append(random.randint(1,10))
 print(list)
-listBackup = list
+
 for a in range(0,len(list)):
-    r = listBackup[a]
-    listBackup.pop(a)
-    sum2 = listBackup[0] * listBackup[1] * listBackup[2] * listBackup[3]
+    r = list[a]
+    list.pop(a)
+    sum2 = list[0] * list[1] * list[2] * list[3]
     list2.append(sum2)
-    listBackup.insert(a, r)
+    list.insert(a, r)
 print(list2)

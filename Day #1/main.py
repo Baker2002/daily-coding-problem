c9bc93fdee8e3e40e6d3d0313fa95c56438880f8
Baker2002 <bakirhimzanija@gmail.com>
2022-09-01 21:27:13 +0200

    Finished

--- Day #1/main.py
+++ Day #1/main.py
@@ -1,1 +1,16 @@
-print("Works?")
+import random
+
+list = []
+for a in range(1,5):
+    list.append(random.randint(4,20))
+
+k = random.randint(16,40)
+print(f"{list}\n{k}")
+
+for b in range(0,len(list)-1):
+    r = k - list[b]
+    if list.count(r) == 1:
+        index = list.index(r)
+        tacanBroj = list[index]
+        print(f"{list[b]} + {tacanBroj} = {k}")
+        break
